c13830bd7806a6b322e1df3972bc0a51129826e2
Elle Meredith <ellemeredith@gmail.com>
2017-05-18 21:56:48 +1000

    Based on previous refactor, changed Move to work with Position as well

--- lib/move.rb
+++ lib/move.rb
@@ -1,8 +1,8 @@
 class Move
-  attr_reader :robot
+  attr_reader :position
 
-  def initialize(robot: robot)
-    @robot = robot
+  def initialize(position: position)
+    @position = position
   end
 
   def go
@@ -12,23 +12,23 @@ class Move
   private
 
   def direction
-    robot.direction
+    position.direction
   end
 
   def go_north
-    robot.y = robot.y + 1
+    position.y = position.y + 1
   end
 
   def go_east
-    robot.x = robot.x + 1
+    position.x = position.x + 1
   end
 
   def go_south
-    robot.y = robot.y - 1
+    position.y = position.y - 1
   end
 
   def go_west
-    robot.x = robot.x - 1
+    position.x = position.x - 1
   end
 end
 
--- test/move_test.rb
+++ test/move_test.rb
@@ -3,39 +3,39 @@ require "test_helper"
 describe Move do
   describe "#go" do
     it "moves north" do
-      robot = Robot.new(x: 0, y: 0, direction: :north)
+      position = Position.new(x: 0, y: 0, direction: :north)
 
-      Move.new(robot: robot).go
+      Move.new(position: position).go
 
-      assert 1, robot.y
-      assert 0, robot.x
+      assert 1, position.y
+      assert 0, position.x
     end
 
     it "moves east" do
-      robot = Robot.new(x: 0, y: 0, direction: :east)
+      position = Position.new(x: 0, y: 0, direction: :east)
 
-      Move.new(robot: robot).go
+      Move.new(position: position).go
 
-      assert 1, robot.x
-      assert 0, robot.x
+      assert 1, position.x
+      assert 0, position.x
     end
 
     it "moves south" do
-      robot = Robot.new(x: 0, y: 1, direction: :south)
+      position = Position.new(x: 0, y: 1, direction: :south)
 
-      Move.new(robot: robot).go
+      Move.new(position: position).go
 
-      assert 0, robot.y
-      assert 0, robot.x
+      assert 0, position.y
+      assert 0, position.x
     end
 
     it "moves west" do
-      robot = Robot.new(x: 1, y: 1, direction: :west)
+      position = Position.new(x: 1, y: 1, direction: :west)
 
-      Move.new(robot: robot).go
+      Move.new(position: position).go
 
-      assert 1, robot.y
-      assert 0, robot.x
+      assert 1, position.y
+      assert 0, position.x
     end
   end
 end
