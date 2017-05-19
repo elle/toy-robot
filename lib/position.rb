c316a258efd6e3c4b740cbd926eaf19f39d0221a
Elle Meredith <ellemeredith@gmail.com>
2017-05-19 11:00:05 +1000

    Refactor Position
    - Added validations
    - Moved DIRECTIONS inside the class because
      really it's the only class that it is relevant for
    - Removed the ability to turn from Position
      because it shouldn't concern itself with it
    - Which made grabbing the left or right directions
      more logical and appropriate.
      So we just report on the directions,
      rather then mutating self by rotating itself.

--- lib/position.rb
+++ lib/position.rb
@@ -3,28 +3,47 @@
 # |
 # +--> X coordinate
 
-DIRECTIONS = [:north, :east, :south, :west]
-
 class Position
+  DIRECTIONS = [:north, :east, :south, :west]
+
   attr_accessor :x, :y, :direction
 
   def initialize(x: 0, y: 0, direction: :north)
     @x, @y, @direction = x, y, direction
+
+    # Consider adding a more useful error message
+    if invalid?
+      raise "Invalid!"
+    end
   end
 
-  def left
-    @direction = DIRECTIONS[index-1]
-    self
+  def left_direction
+    DIRECTIONS[index-1]
   end
 
-  def right
-    @direction = DIRECTIONS[index+1]
-    self
+  def right_direction
+    DIRECTIONS[index+1]
   end
 
   private
 
   def index
     DIRECTIONS.index(direction)
+  end
+
+  def invalid?
+    !valid?
+  end
+
+  def valid?
+    positive_coordinates && valid_direction
+  end
+
+  def positive_coordinates
+    x >= 0 && y >= 0
+  end
+
+  def valid_direction
+    DIRECTIONS.include? direction
   end
 end
--- test/position_test.rb
+++ test/position_test.rb
@@ -1,34 +1,62 @@
 require "test_helper"
 
 describe Position do
-  it "has x and y coordinates" do
-    position = Position.new
-
-    assert_equal 0, position.x
-    assert_equal 0, position.y
+  describe "DIRECTIONS" do
+    it "has 4 cardinal points" do
+      assert_equal [:north, :east, :south, :west], Position::DIRECTIONS
+    end
   end
 
-  it "has direction" do
-    position = Position.new
-
-    assert_equal :north, position.direction
-  end
-
-  describe "#left" do
-    it "rotates to the left" do
+  context "Coordinates" do
+    it "has defaults of south west corner" do
       position = Position.new
-      position.left
+
+      assert_equal 0, position.x
+      assert_equal 0, position.y
+    end
+  end
+
+  context "#direction" do
+    it "defaults to :north" do
+      position = Position.new
+
+      assert_equal :north, position.direction
+    end
+
+    it "sets a direction" do
+      position = Position.new(direction: :west)
 
       assert_equal :west, position.direction
     end
   end
 
-  describe "#right" do
-    it "rotates to the right" do
-      position = Position.new
-      position.right
+  context "valdiations" do
+    it "cannot have negative coordinates" do
+      assert_raises "Invalid!" do
+        Position.new(x: -1)
+      end
+    end
 
-      assert_equal :east, position.direction
+    it "raises an error when direction does not exist" do
+      assert_raises "Invalid!" do
+        Position.new(direction: :nodirection)
+      end
+    end
+  end
+
+  describe "#left_direction" do
+    it "finds the direction to its left" do
+      position = Position.new
+
+      assert_equal :west, position.left_direction
+    end
+  end
+
+  describe "#right_direction" do
+    it "finds its direction to its right" do
+      position = Position.new
+
+      assert_equal :east, position.right_direction
     end
   end
 end
